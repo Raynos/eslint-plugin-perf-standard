1aa4306ef01ffc4bbe9010dbb26d3865e8870490
Raynos <raynos2@gmail.com>
2016-01-09 21:44:52 -0800

    implement no-instanceof-guard

--- test/index.js
+++ test/index.js
@@ -1,10 +1,33 @@
 'use strict';
 
-var test = require('tape');
+var console = require('console');
+var RuleTester = require('eslint').RuleTester;
 
-var eslintPluginPerfStandard = require('../index.js');
+var noInstanceofGuard = require('../rules/no-instanceof-guard.js');
 
-test('eslintPluginPerfStandard is a function', function t(assert) {
-    assert.equal(typeof eslintPluginPerfStandard, 'function');
-    assert.end();
+var ruleTester = new RuleTester();
+
+ruleTester.run('no-instanceof-gaurd', noInstanceofGuard, {
+    valid: [
+        [
+            'function Foo() {',
+            '    this.bar = "bar";',
+            '}'
+        ].join('\n')
+    ],
+    invalid: [{
+        code: [
+            'function Foo() {',
+            '    if (!(this instanceof Foo)) return new Foo();',
+            '    ',
+            '    this.bar = "bar";',
+            '}'
+        ].join('\n'),
+        errors: [{
+            message: 'expected no instanceof gaurd in constructor'
+        }]
+    }]
 });
+
+/*eslint no-console: 0*/
+console.log('ok');
